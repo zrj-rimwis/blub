0c96d45e088f3e6b3664a4ef16ad137bac3074dd
zrj <rimvydas.jasinskas@gmail.com>
2016-02-15 17:33:33 +0200

    libstand: Add faster bswap64() from NetBSD
    Taken-from: NetBSD

--- src/lib/libstand/bswap.c
+++ src/lib/libstand/bswap.c
@@ -3,13 +3,16 @@
  * Public domain.
  *
  * $NetBSD: bswap32.c,v 1.1 1997/10/09 15:42:33 bouyer Exp $
- * $NetBSD: bswap64.c,v 1.1 1997/10/09 15:42:33 bouyer Exp $
+ * $NetBSD: bswap64.c,v 1.3 2009/03/16 05:59:21 cegger Exp $
  */
 
 #include <sys/types.h>
 
 #undef bswap32
 #undef bswap64
+
+u_int32_t bswap32(u_int32_t x);
+u_int64_t bswap64(u_int64_t x);
 
 u_int32_t
 bswap32(u_int32_t x)
@@ -23,10 +26,27 @@ bswap32(u_int32_t x)
 u_int64_t
 bswap64(u_int64_t x)
 {
-	u_int32_t *p = (u_int32_t*)&x;
-	u_int32_t t;
-	t = bswap32(p[0]);
-	p[0] = bswap32(p[1]);
-	p[1] = t;
-	return x;
+#ifdef __LP64__
+	/*
+	 * Assume we have wide enough registers to do it without touching
+	 * memory.
+	 */
+	return  ( (x << 56) & 0xff00000000000000UL ) |
+		( (x << 40) & 0x00ff000000000000UL ) |
+		( (x << 24) & 0x0000ff0000000000UL ) |
+		( (x <<  8) & 0x000000ff00000000UL ) |
+		( (x >>  8) & 0x00000000ff000000UL ) |
+		( (x >> 24) & 0x0000000000ff0000UL ) |
+		( (x >> 40) & 0x000000000000ff00UL ) |
+		( (x >> 56) & 0x00000000000000ffUL );
+#else
+	/*
+	 * Split the operation in two 32bit steps.
+	 */
+	u_int32_t tl, th;
+
+	th = bswap32((u_int32_t)(x & 0x00000000ffffffffULL));
+	tl = bswap32((u_int32_t)((x >> 32) & 0x00000000ffffffffULL));
+	return ((u_int64_t)th << 32) | tl;
+#endif
 }
