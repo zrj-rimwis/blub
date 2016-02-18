a73879bdcc20dd152137771c5de06b978ecdd03f
zrj <rimvydas.jasinskas@gmail.com>
2016-02-18 15:50:40 +0200

    sys/boot: Add common CTASSERT definitions.
    Taken-from: FreeBSD GIT c4c31a5(235153)

--- src/sys/boot/common/bootstrap.h
+++ src/sys/boot/common/bootstrap.h
@@ -26,6 +26,9 @@
  * $FreeBSD: src/sys/boot/common/bootstrap.h,v 1.38 2003/05/01 03:56:29 peter Exp $
  */
 
+#ifndef _BOOTSTRAP_H_
+#define	_BOOTSTRAP_H_
+
 #include <sys/types.h>
 #include <sys/queue.h>
 #include <sys/linker_set.h>
@@ -316,3 +319,11 @@ void	delay(int delay);
 void	dev_cleanup(void);
 
 time_t	time(time_t *tloc);
+
+#ifndef CTASSERT                /* Allow lint to override */
+#define CTASSERT(x)             _CTASSERT(x, __LINE__)
+#define _CTASSERT(x, y)         __CTASSERT(x, y)
+#define __CTASSERT(x, y)        typedef char __assert ## y[(x) ? 1 : -1]
+#endif
+
+#endif /* !_BOOTSTRAP_H_ */
