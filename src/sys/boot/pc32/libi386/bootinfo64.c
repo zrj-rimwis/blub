4c1e5db6b036bd908cad87441dcf38b7d7f57eda
zrj <rimvydas.jasinskas@gmail.com>
2016-02-23 12:05:20 +0200

    sys/boot: Reduce magic strings in bootinfo64.c
    While there, fix regs[4] to use unsigned int.

--- src/sys/boot/pc32/libi386/bootinfo64.c
+++ src/sys/boot/pc32/libi386/bootinfo64.c
@@ -133,7 +133,8 @@ bi_checkcpu(void)
 {
     char *cpu_vendor;
     int vendor[3];
-    int eflags, regs[4];
+    int eflags;
+    unsigned int regs[4];
 
     /* Check for presence of "cpuid". */
     eflags = read_eflags();
@@ -149,9 +150,9 @@ bi_checkcpu(void)
     cpu_vendor = (char *)vendor;
 
     /* Check for vendors that support AMD features. */
-    if (strncmp(cpu_vendor, "GenuineIntel", 12) != 0 &&
-	strncmp(cpu_vendor, "AuthenticAMD", 12) != 0 &&
-	strncmp(cpu_vendor, "CentaurHauls", 12) != 0)
+    if (strncmp(cpu_vendor, INTEL_VENDOR_ID, 12) != 0 &&
+	strncmp(cpu_vendor, AMD_VENDOR_ID, 12) != 0 &&
+	strncmp(cpu_vendor, CENTAUR_VENDOR_ID, 12) != 0)
 	return (0);
 
     /* Has to support AMD features. */
