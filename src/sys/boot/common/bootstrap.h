ce79dc4bcda7bf9a5d96da8af3c7b73c4af911bd
zrj <rimvydas.jasinskas@gmail.com>
2016-04-17 13:17:23 +0300

    efi: Use devdesc struct in blub.
    Add d_opendata member to devdesc.

--- src/sys/boot/common/bootstrap.h
+++ src/sys/boot/common/bootstrap.h
@@ -47,6 +47,7 @@ struct devdesc
 #define DEVT_NET	2
 #define	DEVT_CD		3
     int			d_unit;
+    void		*d_opendata;
 };
 
 /* Commands and return values; nonzero return sets command_errmsg != NULL */
