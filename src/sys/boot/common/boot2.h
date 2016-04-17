d270e746aed52cafb9c58fd8d2abe98015a0c266
zrj <rimvydas.jasinskas@gmail.com>
2016-04-17 15:54:35 +0300

    efi: boot1 changes
    Mostly from gptboot w/o disturbing rest too much.

--- src/sys/boot/common/boot2.h
+++ src/sys/boot/common/boot2.h
@@ -53,7 +53,7 @@
  * We only need a 32 bit ino_t for UFS-only boot code.  We have to squeeze
  * space usage, else we'd just use 64 bits across the board.
  */
-#if defined(GPT) || defined(HAMMERFS) || defined(HAMMER2FS)
+#if defined(EFI_UFS_BOOT) || defined(GPT) || defined(HAMMERFS) || defined(HAMMER2FS)
 typedef uint64_t boot2_ino_t;
 #else
 typedef uint32_t boot2_ino_t;
@@ -74,14 +74,16 @@ struct boot2_dmadat {
 	/* extended by *fsread() modules */
 };
 
+#ifndef EFI_UFS_BOOT
 extern uint32_t fs_off;
 extern int	no_io_error;
+#endif
 extern int	ls;
 extern struct boot2_dmadat *boot2_dmadat;
 
-#ifdef GPT
+#if defined(GPT)
 extern int dskread(void *, daddr_t, unsigned);
-#else
+#elif !defined(EFI_UFS_BOOT)
 extern int dskread(void *, unsigned, unsigned);
 
 extern void printf(const char *,...);
